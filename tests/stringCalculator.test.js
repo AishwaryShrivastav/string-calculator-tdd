19b5000b542667465a7cc6912b658aed4d21cfaf
Aishwarya Shrivastav <aishwarya.shrivastav@aerchain.io>
2025-03-19 14:55:54 +0530

    Added: Test case to handle a single number
    Returns the number itself when a single number is passed

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require("../src/stringCalculator");
 test("returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+test("returns the number itself when a single number is passed", () => {
+    expect(add("1")).toBe(1);
+});
