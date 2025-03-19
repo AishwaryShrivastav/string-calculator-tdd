4c9482d758423c8c36e8996297a8cf503fd7a925
Aishwarya Shrivastav <aishwarya.shrivastav@aerchain.io>
2025-03-19 15:05:25 +0530

    Added: Functioanlity to handle multiple comma seperated numbers
    Added test case to handle multiple comma seperated numbers.
    Improved the function 'add' to handle the test case for managing multiple numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,6 @@
 function add(numbers) {
     if (numbers === "") return 0;
-    const numList = numbers.split(",").map(Number);
-    return numList.reduce((sum, num) => sum + num, 0);
+    return numbers.split(",").map(Number).reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -11,3 +11,7 @@ test("returns the number itself when a single number is passed", () => {
 test("returns the sum of two numbers", () => {
     expect(add("1,2")).toBe(3);
 });
+
+test("returns the sum of multiple numbers", () => {
+    expect(add("1,2,3,4")).toBe(10);
+});
